3f9b833927f4a7245bd2658343b964751dfa9563
dunnkers <jeroenoverschie@gmail.com>
2018-03-23 12:01:55 +0100

    ⚠️ Not working integration

--- pydash-front/src/App.js
+++ pydash-front/src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
 import './App.css';
-import Login from './login/Login';
-import Dashboard from './dashboard/Dashboard';
-import { Switch, Route } from 'react-router-dom'
+import LoginComponent from './login/Login';
+import DashboardComponent from './dashboard/Dashboard';
+import { Switch, Route, NavLink } from 'react-router-dom'
+import { logout } from './actions/user'
+import { connect } from 'react-redux'
+import {
+  userIsAuthenticatedRedir, userIsNotAuthenticatedRedir,
+  userIsAuthenticated, userIsNotAuthenticated
+} from './auth'
+
+
+const getUserName = user => {
+  if (user.data) {
+    return `Welcome ${user.data.name}`
+  }
+  return `Not logged in`
+}
+
+// Need to apply the hocs here to avoid applying them inside the render method
+const Login = userIsNotAuthenticatedRedir(LoginComponent)
+const Dashboard = userIsAuthenticatedRedir(DashboardComponent)
+
+// Only show login when the user is not logged in and logout when logged in
+// Could have also done this with a single wrapper and `FailureComponent`
+const UserName = ({ user }) => (<div>{getUserName(user)}</div>)
+// className={styles.username}
+const LoginLink = userIsNotAuthenticated(() => <NavLink to="/login">Login</NavLink>) 
+// activeClassName={styles.active}
+const LogoutLink = userIsAuthenticated(({ logout }) => <a href="#" onClick={() => logout()}>Logout</a>)
+
 
 class App extends Component {
   state = {
@@ -27,4 +54,8 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user
+})
+
+export default connect(mapStateToProps, { logout })(App)
--- pydash-front/src/index.js
+++ pydash-front/src/index.js
@@ -5,9 +5,37 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 
+// redux
+import { createDevTools } from 'redux-devtools'
+import LogMonitor from 'redux-devtools-log-monitor'
+import DockMonitor from 'redux-devtools-dock-monitor'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
+import { Provider } from 'react-redux'
+import thunkMiddleware from 'redux-thunk'
+import * as reducers from './reducers'
+
+const reducer = combineReducers(Object.assign({}, reducers, {}))
+
+const DevTools = createDevTools(
+    <DockMonitor toggleVisibilityKey="ctrl-h"
+        changePositionKey="ctrl-q">
+        <LogMonitor theme="tomorrow" />
+    </DockMonitor>
+)
+
+const enhancer = compose(
+    // Middleware you want to use in development:
+    applyMiddleware(thunkMiddleware),
+    DevTools.instrument()
+)
+
+const store = createStore(reducer, enhancer)
+
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
 registerServiceWorker();
